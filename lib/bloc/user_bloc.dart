a0690b45b4a6438ea8c0181bca7337e6c40fb083
Luis Angel <luisparr14@gmail.com>
2023-04-13 21:03:40 -0500

    clase #160 - flutter avanzado - Udemy

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
 import '../models/user.dart';
 
 part 'user_events.dart';
 part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvents, UserState> {
+  UserBloc():super(const UserInitialState()) {
+    on<ActivateUser>((event, emit) {
+      
+    });
+  }
+}
--- lib/bloc/user_state.dart
+++ lib/bloc/user_state.dart
@@ -5,8 +5,9 @@ abstract class UserState {
   final bool userExiste;
   final User? user;
 
-  const UserState({
-    this.userExiste = false,
-    this.user
-  });
+  const UserState({this.userExiste = false, this.user});
+}
+
+class UserInitialState extends UserState {
+  const UserInitialState() : super(user: null, userExiste: false);
 }
